f0d3036e0e28d7677187bd2b53ee244c2af2a6a9
Hamza Naeem Khan <hamzanaeemkhan977@gmail.com>
2023-06-04 16:53:13 +0500

    functionality dark light

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,7 +1,20 @@
+import { createContext, useState } from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+export const ThemeContext =   createContext(null)
 
 function App() {
-  return <div className="App"></div>;
+  const [theme, setTheme] = useState("light")
+const toggleTheme = ()=>{
+  setTheme((curr) => (curr === "light" ? "dark" : "light"));
+}
+  return (
+    <ThemeContext.Provider value={{theme , toggleTheme}}>
+  <div className="App" id={theme}>
+    <Sidebar  theme = {theme} setTheme={setTheme} toggleTheme={toggleTheme} />
+  </div>
+     </ThemeContext.Provider>
+  )
 }
 
 export default App;
